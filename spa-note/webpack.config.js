01f3d4464f9ba7dc7fe9288ef0baf10ee21c4954
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-03-30 08:58:40 +0900

    Create application base

---
+++ spa-note/webpack.config.js
@@ -0,0 +1,51 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: { js: __dirname + '/src/main.js', css: __dirname + '/src/main.css' },
+  output: { path: __dirname + '/public', filename: 'bundle.[name]' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  require('postcss-easy-import')({ glob: true }),
+                ],
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    port: 8080,
+    inline: true,
+    historyApiFallback: true,
+    stats: {
+      version: false,
+      hash: false,
+      chunkModules: false,
+    },
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      disable: false,
+      allChunks: true,
+    }),
+  ],
+  devtool: 'source-map',
+}
